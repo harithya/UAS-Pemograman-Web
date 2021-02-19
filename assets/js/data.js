da331df275988a8fc9d532df843241cb8ddebb06
Harithya Wisesa <harithya77@gmail.com>
2021-02-19 08:54:21 +0700

    Membuat section service

---
+++ assets/js/data.js
@@ -0,0 +1,33 @@
+const services = [
+    {
+        icon: 'ti ti-eraser',
+        title: 'Layanan Pendidikan',
+        description: 'kami berusaha keras melakukan pelayanan pendidikan yeng maksimal.'
+    },
+    {
+        icon: 'ti ti-paint-roller',
+        title: 'Layanan Ekonomi',
+        description: 'Pembangunan fasilitas yang layak bagi penduduk.'
+    },
+    {
+        icon: 'ti ti-support',
+        title: 'Layanan Kesehatan',
+        description: 'kami berusaha keras memberikan layanan kesehatan sebaik mungkin.'
+    },
+    {
+        icon: 'ti ti-marker-alt',
+        title: 'Pembuatan Surat',
+        description: 'Layanan administrasi surat menjadi hal paling sering dilakukan di pemerintah desa.'
+    },
+    {
+        icon: 'ti ti-stats-up',
+        title: 'Kependudukan',
+        description: 'kami selalu melakukan yang terbaik dan menggunakan teknologi yang paling tepat.'
+    },
+    {
+        icon: 'ti ti-light-bulb',
+        title: 'Perijinan',
+        description: 'kami selalu melakukan yang terbaik dan menggunakan teknologi yang paling tepat.'
+    },
+
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,23 @@ $(document).ready(function () {
             $(".navbar-navigation").addClass('menu-active');
         }
     })
+
+    const renderService = () => {
+        let html = ``;
+        for (let i = 0; i < services.length; i++) {
+            html += `<div class="media">
+                <div class="media-icon">
+                    <i class="${services[i].icon}"></i>
+                </div>
+                <div class="media-body">
+                    <h4>${services[i].title}</h4>
+                    <p>${services[i].description}</p>
+                </div>
+            </div>`
+        }
+        $('.media-grid').html(html)
+    }
+
+    renderService()
+
 })
